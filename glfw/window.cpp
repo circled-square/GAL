f5348be9c9f5b813735489819f98178b218da7e7
ordinatrice <circled-square@github.com>
2021-10-04 22:40:51 +0200

    glfw RAII helper

---
+++ glfw/window.cpp
@@ -0,0 +1,54 @@
+#include "window.hpp"
+#include <stdexcept>
+
+namespace glfw {
+    std::int64_t window::window_count = 0;
+    void window::create(std::pair<int,int> res, std::string title) {
+        if(m_window_ptr)
+            this->destroy();
+        //Initialize the library 
+        if(!window_count) {
+            if(!glfwInit()) {
+                throw std::runtime_error("Error initialising GLFW3\n");
+            } else {
+                //make sure your system has opengl 4.4 before using these hints
+                glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+                glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+                glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+            }
+        }
+        /* Create a windowed mode window and its OpenGL context */
+        GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+        const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+        if (!monitor || !mode)
+            throw std::runtime_error("Error in finding primary monitor (GLFW3) or associated video mode\n");
+        
+        glfwWindowHint(GLFW_RED_BITS,     mode->redBits);
+        glfwWindowHint(GLFW_GREEN_BITS,   mode->greenBits);
+        glfwWindowHint(GLFW_BLUE_BITS,    mode->blueBits);
+        glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);
+
+        if(res.first < 0 || res.second < 0)
+            m_window_ptr = glfwCreateWindow(mode->width, mode->height, title.c_str(), monitor, nullptr);
+        else
+            m_window_ptr = glfwCreateWindow(res.first, res.second, title.c_str(), nullptr, nullptr);
+
+        if (!m_window_ptr)
+            throw std::runtime_error("Error creating GLFW3 window\n");
+        #ifdef USE_OPENGL
+        glfwSetFramebufferSizeCallback(m_window_ptr, [](window_t, int W, int H) { glViewport(0, 0, W, H); } );
+        #endif
+        glfwMakeContextCurrent(m_window_ptr);
+
+        window::window_count++;
+    }
+
+    void window::destroy() {
+        glfwDestroyWindow(m_window_ptr);
+        //if no windows are left terminate glfw
+        if(!--window::window_count)
+            glfwTerminate();
+        m_window_ptr = nullptr;
+    }
+}
+
---
+++ glfw/window.hpp
@@ -0,0 +1,70 @@
+#ifndef GLFW_WRAPPER_HPP
+#define GLFW_WRAPPER_HPP
+/*
+window, a RAII and OO wrapper for glfw windows, thread unsafe because glfw is(unfortunately).
+Construction and destruction may only be invoked in the main thread but some other functions *might* be 
+callable from other threads. 
+*/
+#include <string>
+#include <array>
+#include <cstdint>
+#include <GLFW/glfw3.h>
+#include <scluk/functional.hpp>
+
+#ifdef USE_OPENGL
+extern void glViewport(std::int32_t x, std::int32_t y, std::uint32_t w, std::uint32_t h);
+#endif
+
+namespace glfw {
+    using window_t = GLFWwindow*;
+    class window {
+        static std::int64_t window_count;
+        window_t m_window_ptr = nullptr;
+    public:
+        void create(std::pair<int,int> res, std::string title);
+        void destroy();
+        bool should_close     () { return glfwWindowShouldClose(m_window_ptr); }
+        void swap_buffers     () { glfwSwapBuffers(m_window_ptr); }
+        void make_ctx_current () { glfwMakeContextCurrent(m_window_ptr); }
+        bool get_key          (int key) { return glfwGetKey        (m_window_ptr, key) != GLFW_RELEASE; }
+        bool get_mouse_btn    (int btn) { return glfwGetMouseButton(m_window_ptr, btn) != GLFW_RELEASE; }
+        void get_cursor_xy    (double *x, double *y) { glfwGetCursorPos(m_window_ptr, x, y); }
+        void get_framebuf_sz  (int *w, int *h) { glfwGetFramebufferSize(m_window_ptr, w, h); }
+
+        #ifdef GLM_VERSION
+        glm::dvec2 get_cursor_pos() {
+            glm::dvec2 ret;
+            glfwGetCursorPos(m_window_ptr, &ret.x, &ret.y);
+            return ret;
+        }
+        
+        glm::ivec2 get_framebuf_sz() {
+            glm::ivec2 ret;
+            glfwGetFramebufferSize(m_window_ptr, &ret.x, &ret.y);
+            return ret;
+        }
+        #endif
+
+        void set_resize_cb (GLFWwindowsizefun f) { glfwSetWindowSizeCallback(m_window_ptr, f); }
+        void set_key_cb (GLFWkeyfun f) { glfwSetKeyCallback(m_window_ptr, f); }
+        void set_key_cb (auto f) {
+            set_key_cb(scluk::lambda_to_fnptr<void(window_t,int,int,int,int)>(f));
+        }
+        
+        static void  poll_events () { glfwPollEvents(); }
+        
+        //constructors/destructors that just resort to calling the previous functions + default constructor
+        window () = default;
+        window (std::pair<int,int> res, const std::string& title) { this->create(res,title); }
+        window (int w, int h, const std::string& title) { this->create(std::make_pair(w,h), title); }
+        ~window () { this->destroy(); }
+        
+        //access and conversion of the internal GLFWwindow*
+        operator void*    () { return  m_window_ptr; }
+        operator window_t () { return  m_window_ptr; }
+        operator bool     () { return  m_window_ptr; }
+        bool operator !        () { return !m_window_ptr; }
+    };
+}
+#endif //GLFW_WRAPPER_HPP
+
