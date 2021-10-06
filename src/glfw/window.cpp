8b0b6195659816b87176afa1f824d0b151ccbe81
ordinatrice <circled-square@github.com>
2021-10-06 17:39:46 +0200

    now depending on scluk for exceptions; not hinting the OpenGL version anymore as that caused a black screen with seemingly no other side effects; added function throw_on_error which made error handling much more succint; made all constructors depend on glfw::window::window() so the window ptr is never used uninitialised

--- src/glfw/window.cpp
+++ src/glfw/window.cpp
@@ -1,31 +1,31 @@
 #include "window.hpp"
-#include <stdexcept>
+#include <scluk/exception.hpp>
+#include <source_location>
 
 #ifdef USE_OPENGL
 extern void glViewport(std::int32_t x, std::int32_t y, std::uint32_t w, std::uint32_t h);
 #endif
 
 namespace glfw {
+#ifndef __clang__
+    inline auto throw_on_error(auto res, const char* what, std::source_location l = std::source_location::current()) {
+        if(!res) throw scluk::runtime_error("%:%, %\n\t%", l.file_name(), l.line(), l.function_name(), what);
+        return res;
+    }
+#else
+    #define throw_on_error(res, what) ([&]{ auto r = res; if(!r) throw scluk::runtime_error("%:%, %\n\t%", __FILE__, __LINE__, __func__, what); return r; }())
+#endif
     std::int64_t window::window_count = 0;
     void window::create(std::pair<int,int> res, std::string title) {
         if(m_window_ptr)
             this->destroy();
         //Initialize the library 
-        if(!window_count) {
-            if(!glfwInit()) {
-                throw std::runtime_error("Error initialising GLFW3\n");
-            } else {
-                //make sure your system has opengl 4.4 before using these hints
-                glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-                glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
-                glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-            }
-        }
+        if(!window_count)
+            throw_on_error(glfwInit(), "Unable to initialize glfwInit");
+    
         /* Create a windowed mode window and its OpenGL context */
-        GLFWmonitor* monitor = glfwGetPrimaryMonitor();
-        const GLFWvidmode* mode = glfwGetVideoMode(monitor);
-        if (!monitor || !mode)
-            throw std::runtime_error("Error in finding primary monitor (GLFW3) or associated video mode\n");
+        GLFWmonitor* monitor = throw_on_error(glfwGetPrimaryMonitor(), "Unable to find primary monitor");
+        const GLFWvidmode* mode = throw_on_error(glfwGetVideoMode(monitor), "Unable to get video mode");
         
         glfwWindowHint(GLFW_RED_BITS,     mode->redBits);
         glfwWindowHint(GLFW_GREEN_BITS,   mode->greenBits);
@@ -37,8 +37,7 @@ namespace glfw {
         else
             m_window_ptr = glfwCreateWindow(res.first, res.second, title.c_str(), nullptr, nullptr);
 
-        if (!m_window_ptr)
-            throw std::runtime_error("Error creating GLFW3 window\n");
+        throw_on_error(m_window_ptr, "Unable to create GLFW3 window\n");
         #ifdef USE_OPENGL
         glfwSetFramebufferSizeCallback(m_window_ptr, [](window_t, int W, int H) { glViewport(0, 0, W, H); } );
         #endif
