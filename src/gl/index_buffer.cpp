f8c7d3848f54fbe910a8e8af742c784c1d460d25
circled-square <circled-square@github.com>
2023-07-29 15:43:53 +0200

    index_buffer now supports indices of 16 bits (not just 32)

--- src/gl/index_buffer.cpp
+++ src/gl/index_buffer.cpp
@@ -3,15 +3,19 @@
 using namespace glm;
 
 namespace gl {
-    index_buffer::index_buffer(buffer buf, size_t size) : m_buf(std::move(buf)), m_triangle_count(size) {}
+    index_buffer::index_buffer(buffer buf, size_t size, int element_typeid) : m_buf(std::move(buf)), m_triangle_count(size), m_element_typeid(element_typeid) {}
 
-    index_buffer::index_buffer(index_buffer &&o) noexcept: index_buffer(std::move(o.m_buf), o.m_triangle_count) {}
+    index_buffer::index_buffer(index_buffer &&o) noexcept: index_buffer(std::move(o.m_buf), o.m_triangle_count, o.m_element_typeid) {}
 
-    index_buffer::index_buffer(const uvec3* data, size_t size, buffer_creation_params params) : index_buffer(buffer(data, size*sizeof(uvec3), params), size) { }
+    index_buffer::index_buffer(const uvec3* data, size_t size, buffer_creation_params params) : index_buffer(buffer(data, size*sizeof(uvec3), params), size, GL_UNSIGNED_INT) { }
 
-    index_buffer::index_buffer(size_t size, buffer_creation_params params) : index_buffer(nullptr, size, params) { }
+    index_buffer::index_buffer(const u16vec3* data, size_t size, buffer_creation_params params) : index_buffer(buffer(data, size*sizeof(u16vec3), params), size, GL_UNSIGNED_SHORT) { }
+
+    index_buffer::index_buffer(int element_typeid, size_t size, buffer_creation_params params) : index_buffer(buffer(nullptr, size, params), size, element_typeid) { }
 
     uint index_buffer::get_gl_id() const { return m_buf.get_gl_id(); }
+
+    uint index_buffer::get_element_typeid() const { return m_element_typeid; }
 
     uint index_buffer::get_triangle_count() const { return m_triangle_count; }
 }
--- src/gl/index_buffer.hpp
+++ src/gl/index_buffer.hpp
@@ -3,32 +3,53 @@
 
 #include "buffer.hpp"
 #include <glm/glm.hpp>
+#include <scluk/metaprogramming.hpp>
+
 
 namespace gl {
-    template<typename T>
-    concept array_of_uvec3 = scluk::concepts::indexable<T> && std::same_as<typename T::value_type, glm::uvec3>;
+    template<typename T, typename U>
+    concept array_of = scluk::concepts::indexable<T> && std::same_as<typename T::value_type, U>;
 
     class index_buffer {
         buffer m_buf;
         size_t m_triangle_count; // store the size of the buffer to be passed to opengl for draw calls  (unless only part of the ibo is read)
-        static constexpr size_t stride = 3 * sizeof(uint);
+        int m_element_typeid;//GL_UNSIGNED_INT or GL_UNSIGNED_SHORT
+
+        size_t stride() {
+            return 3 * (
+                m_element_typeid == GL_UNSIGNED_INT ? sizeof(uint) :
+                m_element_typeid == GL_UNSIGNED_SHORT ? sizeof(unsigned short) : 0
+            );
+        }
     public:
         index_buffer(index_buffer&& o)  noexcept;
-        index_buffer(buffer buf, size_t size);
+        index_buffer(buffer buf, size_t size, int element_typeid);
 
-        template<array_of_uvec3 arr_t>
-        index_buffer(const arr_t& arr, buffer_creation_params params = {}) : index_buffer(buffer(arr, params), arr.size()) { }
+        template<array_of<glm::uvec3> arr_t>
+        index_buffer(const arr_t& arr, buffer_creation_params params = {}) : index_buffer(buffer(arr, params), arr.size(), GL_UNSIGNED_INT) { }
+
+        template<array_of<glm::u16vec3> arr_t>
+        index_buffer(const arr_t& arr, buffer_creation_params params = {}) : index_buffer(buffer(arr, params), arr.size(), GL_UNSIGNED_SHORT) { }
 
         //pass in the number of uvec3, NOT the bytes
         index_buffer(const glm::uvec3* data, size_t size, buffer_creation_params params = {});
-        index_buffer(size_t size, buffer_creation_params params = {});
+        index_buffer(const glm::u16vec3* data, size_t size, buffer_creation_params params = {});
+        index_buffer(int element_typeid, size_t size, buffer_creation_params params = {});
 
-        template<array_of_uvec3 arr_t>
+        template<array_of<glm::uvec3> arr_t>
         void update(const arr_t& arr, size_t offset = 0) {
-            m_buf.update(offset, arr.data(), arr.size() * stride);
+            assert(m_element_typeid == GL_UNSIGNED_INT);
+            m_buf.update(offset, arr.data(), arr.size() * stride());
+        }
+        template<array_of<glm::u16vec3> arr_t>
+        void update(const arr_t& arr, size_t offset = 0) {
+            assert(m_element_typeid == GL_UNSIGNED_SHORT);
+            m_buf.update(offset, arr.data(), arr.size() * stride());
         }
 
+
         uint get_gl_id() const;
+        uint get_element_typeid() const;
         uint get_triangle_count() const;
     };
 } // gl
