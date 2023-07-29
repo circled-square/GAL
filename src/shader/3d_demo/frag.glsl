c40d82b4e78869c9d703f554a1c68a6e5da285c1
circled-square <circled-square@github.com>
2023-07-29 15:46:48 +0200

    removed debug stuff

--- src/shader/3d_demo/frag.glsl
+++ src/shader/3d_demo/frag.glsl
@@ -1,14 +1,12 @@
 #version 330 core
+//3d demo fragment shader
 
 out vec4 color;
 
 uniform sampler2D u_texture_slot;
 
 in vec2 v_tex_coord;
-in float dbg;
 
 void main() {
-   color = texture(u_texture_slot, v_tex_coord);
-   //color =vec4(v_tex_coord, .3, 1.);
-   //color.z = dbg;
+    color = texture(u_texture_slot, v_tex_coord);
 }
