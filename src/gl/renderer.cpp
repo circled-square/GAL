2074e629d1995a595786922c0ca74d828c378df7
circled-square <circled-square@github.com>
2023-07-29 15:44:29 +0200

    renderer now supports index_buffer with indices of 16 bits (not just 32)

--- src/gl/renderer.cpp
+++ src/gl/renderer.cpp
@@ -3,16 +3,19 @@
 //
 
 #include "renderer.hpp"
+#include "types.hpp"
 
 namespace gl {
     void renderer::draw(const vertex_array& vao, const shader_program& shader, uint ibo_index) {
         draw(vao, shader, 0, vao.get_triangle_count(ibo_index), ibo_index);
     }
-    void renderer::draw(const vertex_array &vao, const shader_program &shader, size_t ibo_start, size_t ibo_count, uint ibo_index) {
+    void renderer::draw(const vertex_array& vao, const shader_program& shader, size_t ibo_start, size_t ibo_count, uint ibo_index) {
         shader.bind();
         vao.bind(ibo_index);
+        uint ibo_elem_typeid = vao.get_ibo_element_typeid(ibo_index);
+        uint ibo_elem_size = typeid_to_size(ibo_elem_typeid);
 
-        glDrawElements(GL_TRIANGLES, ibo_count * 3, GL_UNSIGNED_INT, (const void*)(sizeof(uint) * 3 * ibo_start)); //draw triangles({1}), read {2} elements of type {3} starting from the {4}th byte from the bound GL_ELEMENT_ARRAY_BUFFER
+        glDrawElements(GL_TRIANGLES, ibo_count * 3, ibo_elem_typeid, (const void*)(ibo_elem_size * 3 * ibo_start)); //draw triangles({1}), read {2} elements of type {3} starting from the {4}th byte from the bound GL_ELEMENT_ARRAY_BUFFER
 
         shader.unbind();
         vao.unbind();
