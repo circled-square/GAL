43e7229a1ef18366e0720fbbce4c1d40b360945c
circled-square <circled-square@github.com>
2024-03-11 20:04:09 +0100

    added single_shader_renderer

---
+++ include/GAL/graphics/renderer/single_shader_renderer.cpp
@@ -0,0 +1,24 @@
+#include "single_shader_renderer.hpp"
+#include "../../internal/graphics/types.hpp"
+
+#include <scluk/log.hpp>
+
+namespace gal::graphics {
+    single_shader_renderer::single_shader_renderer(shader_program shader) 
+        : m_shader(std::move(shader)) {}
+
+    void single_shader_renderer::draw(const vertex_array& vao, uint ibo_index) {
+        m_renderer.draw(vao, m_shader, ibo_index);
+    }
+    void single_shader_renderer::draw(const vertex_array& vao, size_t ibo_start, size_t ibo_count, uint ibo_index) {
+        m_renderer.draw(vao, m_shader, ibo_start, ibo_count, ibo_index);
+    }
+
+    void single_shader_renderer::clear() {
+        m_renderer.clear();
+    }
+
+    void single_shader_renderer::set_clear_color(glm::vec4 c) {
+        m_renderer.set_clear_color(c);
+    }
+}
---
+++ include/GAL/graphics/renderer/single_shader_renderer.hpp
@@ -0,0 +1,20 @@
+#ifndef GAL_GRAPHICS_SINGLE_SHADER_RENDERER_HPP
+#define GAL_GRAPHICS_SINGLE_SHADER_RENDERER_HPP
+
+#include "../vertex_array/vertex_array.hpp"
+#include "../shader/shader.hpp"
+
+namespace gal::graphics {
+    class single_shader_renderer {
+        shader_program m_shader;
+        renderer m_renderer;
+    public:
+        single_shader_renderer(shader_program shader);
+        void draw(const vertex_array& vao, uint ibo_index = 0);
+        void draw(const vertex_array& vao, size_t ibo_start, size_t ibo_count, uint ibo_index = 0);
+        void clear();
+        void set_clear_color(glm::vec4 c);
+    };
+}
+
+#endif //GAL_GRAPHICS_SINGLE_SHADER_RENDERER_HPP
