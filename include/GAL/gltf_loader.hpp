4644ce7233af1143bfb49246f51c6af2a9a91431
circled-square <46634383+circled-square@users.noreply.github.com>
2024-10-04 16:30:37 +0200

    removed retro_renderer and gltf_loader

--- include/GAL/gltf_loader.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef GAL_GLTF_LOADER_HPP
-#define GAL_GLTF_LOADER_HPP
-
-#include <GAL/application/retro_3d_scene/retro_3d_scene.hpp>
-
-gal::application::retro::node load_node_tree_from_gltf(const char* filepath, const char* node_name = nullptr);
-
-#endif // GAL_GLTF_LOADER_HPP
